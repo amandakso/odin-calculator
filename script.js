101289693f535ea62b29a51f841f5cbb79e5d454
Amanda So <soamanda95@gmail.com>
2022-03-08 11:40:23 -0800

    add functions for basic math operations(+,-,*,/)

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(a,b) {
+    let c= a + b;
+    console.log(c);
+};
+
+function subtract(a,b) {
+    let c=a - b;
+    console.log(c);
+};
+
+function multiply(a,b) {
+    let c= a * b;
+    console.log(c);
+}
+
+function divide(a,b) {
+    let c= a / b;
+    console.log(c);
+};
+add(2,10);
+subtract(2,10);
+multiply(3,5);
+divide(5,2);
+
+
